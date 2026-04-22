52667cfef22742b4a4e33e645ee788cd50b1153e
Piyali Das <piyalidas.it@gmail.com>
2026-04-22 22:21:01 +0530

    Add webpack development configuration file

---
+++ config/webpack.dev.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'eval-cheap-module-source-map',
+
+  devServer: {
+    static: './dist',
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
