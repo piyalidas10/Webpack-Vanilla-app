9c785b179319797a5f4cbc1aea5c4079b379d1cc
Piyali Das <piyalidas.it@gmail.com>
2026-04-22 22:20:40 +0530

    Create webpack.prod.js

---
+++ config/webpack.prod.js
@@ -0,0 +1,43 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+
+    runtimeChunk: 'single',
+  },
+});
